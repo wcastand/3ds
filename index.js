c4ae48a3f442327fea2a7263b8908986fede607f
wcastand <william@paybase.io>
2019-07-15 13:01:10 +0100

    feature(3ds2): start working on 3ds v2

--- index.js
+++ index.js
@@ -1,81 +1,94 @@
-const crypto = require('crypto')
-const { stringify, parse } = require('querystring')
-const FormData = require('form-data')
-const fetch = require('node-fetch')
-const uuid = require('uuid/v4')
-const { send, json, text, buffer } = require('micro')
-const { router, get, post } = require('microrouter')
 const h = require('vhtml')
+const uuid = require('uuid/v4')
+const crypto = require('crypto')
+const fetch = require('node-fetch')
 const html = require('htm').bind(h)
+const FormData = require('form-data')
+const { stringify, parse } = require('querystring')
+const { router, get, post } = require('microrouter')
+const { send, json, text, buffer } = require('micro')
 
-const key = 'Circle4Take40Idea'
-const cardstream_int_url = 'https://gateway.cardstream.com/direct/'
+const key = 'Threeds2Test60System'
+const cardstream_int_url = 'https://test.3ds-pit.com/direct/'
 const cardstream_merchant_id_3ds = '100856'
-const test_card = {
-  number: '4929421234600821',
-  cvv: 356,
-  expirem: 12,
-  expirey: 19,
-  address: `Flat 6 Primrose Rise 347 Lavender Road Northampton`,
-  postcode: 'NN17 8YG',
-}
 const transactionUnique = uuid()
-const test_data = (threeDSMD, threeDSPaRes) => ({
-  action: 'VERIFY',
-  amount: 0,
-  cardCVV: test_card.cvv,
-  cardExpiryMonth: test_card.expirem,
-  cardExpiryYear: test_card.expirey,
-  cardNumber: test_card.number,
-  countryCode: 'GB',
-  currencyCode: 'GBP',
-  customerAddress: test_card.address.replace(/ /g, ''),
-  customerPostCode: test_card.postcode.replace(/ /g, ''),
+
+const new_test_data = callback => ({
   merchantID: cardstream_merchant_id_3ds,
-  ...(threeDSMD && { threeDSMD }),
-  ...(threeDSPaRes && { threeDSPaRes }),
-  threeDSRequired: 'Y',
-  transactionUnique,
+  action: 'SALE',
   type: 1,
+  currencyCode: 826,
+  countryCode: 826,
+  amount: '1.01',
+  cardNumber: '4929421234600821',
+  cardCVV: '356',
+  cardExpiryMonth: '12',
+  cardExpiryYear: '19',
+  customerName: 'Test Customer',
+  customerEmail: 'test@testcustomer.com',
+  customerAddress: '16 Test Street',
+  customerPostCode: 'TE15 5ST',
+  orderRef: 'Test purchase',
+  threeDSRedirectURL: callback,
+  deviceType: 'desktop',
+  deviceChannel: 'browser',
+  deviceTimeZone: '0',
+  deviceCapabilities: 'javascript',
+  deviceScreenResolution: '1920x1080x1',
+  deviceOperatingSystem: 'win',
+  transactionUnique,
 })
 
 const generateBody = (SIGNATURE_KEY, obj) => {
-  const body = stringify(obj)
+  var items = Object.keys(obj)
+  var string = ''
+  items.sort()
+  items.forEach(function(item) {
+    string += item + '=' + encodeURIComponent(obj[item]) + '&'
+  })
+  string = string.slice(0, -1)
+  string = string.replace(/\(/g, '%28')
+  string = string.replace(/\)/g, '%29')
+  string = string.replace(/%20/g, '+')
   return (
-    body +
+    string +
     '&signature=' +
     crypto
       .createHash('SHA512')
-      .update(body + SIGNATURE_KEY)
+      .update(string + SIGNATURE_KEY)
       .digest('hex')
   )
 }
 
 const callc = params => {
   const body = generateBody(key, params)
+  console.log('TCL: body', body)
   return fetch(cardstream_int_url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(body),
+      'Content-Length': body.length,
     },
     body,
   })
     .then(res => res.buffer())
     .then(buffer => buffer.toString('utf8'))
+    .then(r => console.log(r) || r)
     .then(str => parse(str))
 }
 
 const callback = async (req, res) => {
   const parsedRes = parse(await text(req))
-  return await callc(test_data(parsedRes.MD, parsedRes.PaRes))
+  const callback = `http://${req.headers.host}/callback`
+  return await callc(new_test_data(callback, parsedRes.MD, parsedRes.PaRes))
     .then(async data => {
       if (data.responseCode === '0') return send(res, 200, 'Transaction successfull')
       else return send(res, 200, data)
     })
     .catch(e => send(res, 500, e.message))
 }
-const send3DS = async (res, { uri, MD, PaReq, TermUrl }) => {
+
+const send3DS = async (res, { threeDSURL, PaReq, threeDSRedirectURL }) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
   res.end(html`
     <html>
@@ -90,10 +103,8 @@ const send3DS = async (res, { uri, MD, PaReq, TermUrl }) => {
         <div>
           <h1>HTM λ News</h1>
           <p>We need 3D secure auth from you.</p>
-          <form action="${uri}" method="POST">
-            <input type="hidden" name="MD" value="${MD}" />
+          <form action="${threeDSURL}" method="POST">
             <input type="hidden" name="PaReq" value="${PaReq}" />
-            <input type="hidden" name="TermUrl" value="${TermUrl}" />
             <button type="submit">Continue</button>
           </form>
         </div>
@@ -103,17 +114,15 @@ const send3DS = async (res, { uri, MD, PaReq, TermUrl }) => {
 }
 
 const index = async (req, res) => {
-  console.log('index')
   const callback = `http://${req.headers.host}/callback`
-  await callc(test_data())
+  await callc(new_test_data(callback))
     .then(async data => {
       console.log(data)
       if (data.responseCode === '65802')
         send3DS(res, {
-          uri: data.threeDSACSURL,
-          MD: data.threeDSMD,
-          PaReq: data.threeDSPaReq,
-          TermUrl: callback,
+          threeDSURL: data.threeDSURL,
+          PaReq: data['threeDSRequest[PaReq]'],
+          threeDSRedirectURL: data.threeDSRedirectURL,
         })
       else send(res, 200, data)
     })
